f1f5d71a073580a0b980e5ed462a443de477f29d
d13h14 <d13h14@gmail.com>
2014-06-04 00:39:09 -0400

    Revert "Initial Commit"
    This reverts commit ba840e5307281a345405acc065fa46208848f21b.

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysql_connect("localhost", "root", "test123") or die("Could not connect.");
-if(!$db) 
-    die("no db");
-if(!mysql_select_db("adwords",$db))
-    die("No database selected.");
-?>
